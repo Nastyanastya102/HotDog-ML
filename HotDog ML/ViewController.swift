8fb1594b8edb53faddc02475e048801c1656831f
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-20 19:28:14 -0700

    Initial Commit

---
+++ HotDog ML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDog ML
+//
+//  Created by Nastya Klyashtorna on 2024-10-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
